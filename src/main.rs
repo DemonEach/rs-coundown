9f87e012802b457f4a7e8c9b3694b9dadfcd0fb5
Korenev Dmitry <koreneffdmitrii@gmail.com>
2021-04-08 20:48:43 +0500

    Some preparations

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, process::Output, time::Instant};
 use std::thread::sleep;
 use std::time;
 use std::process::Command;
+use std::env;
+
+/// ConsoleCleaner 
+struct ConsoleCleaner {
+    cleaner: Output
+}
+
+impl ConsoleCleaner {
+    pub fn new(&mut self) {
+        //TODO: add cleanup based of OS
+        //println!("{}", env::consts::OS);
+        self.cleaner = Command::new("clear").output().unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+        });
+    }
+}
+
+struct Timer {
+    timer: Instant
+}
 
 fn main() {
-    
+
+    sleep(time::Duration::from_secs(1));
+
     let output = Command::new("clear").output().unwrap_or_else(|e| {
         panic!("failed to execute process: {}", e)
     });
@@ -14,7 +36,6 @@ fn main() {
         println!("I can clear the screen! {}", i);
         sleep(time::Duration::from_secs(1));
         
-      
         println!("{}", String::from_utf8_lossy(&output.stdout));
       
         println!("...see");
