45656a4ab397cb9b1fee9c948e2b639f697dcadd
Korenev Dmitry <koreneffdmitrii@gmail.com>
2021-04-08 16:29:39 +0500

    Some test things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io::{self, Write};
+use std::thread::sleep;
+use std::time;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let output = Command::new("clear").output().unwrap_or_else(|e| {
+        panic!("failed to execute process: {}", e)
+    });
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+    for i in 1..10 {
+        println!("I can clear the screen! {}", i);
+        sleep(time::Duration::from_secs(1));
+        
+      
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+      
+        println!("...see");
+        sleep(time::Duration::from_secs(1));
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+    }
 }
