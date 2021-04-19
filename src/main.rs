1f6314f88b329751d83210c3e39dca4377a45f5f
Korenev Dmitry <koreneffdmitrii@gmail.com>
2021-04-19 18:20:56 +0500

    added timer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{self, Write}, process::Output, time::Instant};
+use std::{fmt::format, process::Output};
 use std::thread::sleep;
 use std::time;
 use std::process::Command;
@@ -20,26 +20,55 @@ impl ConsoleCleaner {
 }
 
 struct Timer {
-    timer: Instant
+    secs: u64
+}
+
+impl Timer {
+    pub fn new(time: String) -> Timer{
+        let times: Vec<u64> = time.split(":").map(|x| x.trim()).filter(|x| !x.is_empty()).map(|x| x.parse().unwrap()).collect();
+        println!("Parsed: {:?}", times);
+        let mut parsed_secs = 0;
+        parsed_secs += times[0] * 3600;
+        parsed_secs += times[1] * 60;
+        parsed_secs += times[2];
+
+        return Timer {
+            secs: parsed_secs
+        };
+    }
+
+    pub fn tick(&mut self) -> String {
+        self.secs -= 1;
+        return self.str();
+    }
+
+    pub fn str(&mut self) -> String {
+        let hours = ((self.secs / 3600) as f64).floor();
+        self.secs %= 3600;
+        let min = ((self.secs / 60) as f64).floor();
+        let sec = ((self.secs % 60) as f64).floor();
+          
+        return format!("{:>0zero$}:{:>0zero$}:{:>0zero$}", hours, min, sec, zero=2);
+    }
+
+    pub fn is_ended(&self) -> bool {
+        return self.secs == 0;
+    }
 }
 
 fn main() {
-
-    sleep(time::Duration::from_secs(1));
 
     let output = Command::new("clear").output().unwrap_or_else(|e| {
         panic!("failed to execute process: {}", e)
     });
 
-    println!("{}", String::from_utf8_lossy(&output.stdout));
-    for i in 1..10 {
-        println!("I can clear the screen! {}", i);
+    //println!("{}", String::from_utf8_lossy(&output.stdout));
+    
+    let mut timer = Timer::new("00:01:00".to_string());
+
+    while !timer.is_ended() {
+        println!("{}", String::from_utf8_lossy(&output.stdout)); // console clear
+        println!("{}", timer.tick());  
         sleep(time::Duration::from_secs(1));
-        
-        println!("{}", String::from_utf8_lossy(&output.stdout));
-      
-        println!("...see");
-        sleep(time::Duration::from_secs(1));
-        println!("{}", String::from_utf8_lossy(&output.stdout));
     }
 }
